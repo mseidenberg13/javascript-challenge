3d2dcd3ac02dbabd68c34a36421fc76d381319a5
Michael Seidenberg <mseidenberg13@gmail.com>
2020-06-09 09:42:22 -0700

    adding completed files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
 var tableData = data;
+console.log(tableData)
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+ 
+  var button = d3.select("#filter-btn");
+  button.on("click", function() {
+      
+    d3.select("tbody").html("");
+    d3.event.preventDefault();
+  
+      tbody.html("");
+      var date = d3.select("#datetime");
+      var input = date.property("value");
+      console.log(input);
+      var filteredData = tableData.filter(ufo => ufo.datetime === input);
+      console.log(filteredData);
+  
+      filteredData.forEach(function(select) {
+      console.log(select);
+
+      var row = tbody.append("tr");
+      Object.entries(select).forEach(function([key, value]) {
+          var cell = row.append("td");
+          cell.text(value);
+      });
+  });
+  });
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
 var tableData = data;
+console.log(tableData)
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    
+    d3.select("tbody").html("");
+    d3.event.preventDefault();
+
+    var date = d3.select("#datetime").property("value");
+    var country = d3.select("#country").property("value").toLowerCase();
+    var state = d3.select("#state").property("value").toLowerCase();
+    var city = d3.select("#city").property("value").toLowerCase();
+    var shape = d3.select("#shape").property("value").toLowerCase();
+
+    filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(ufo => ufo.datetime === date);
+    }
+    if (country) {
+        filteredData = filteredData.filter(ufo => ufo.country === country);
+    }
+    if (state) {
+        filteredData = filteredData.filter(ufo => ufo.state === state);
+    }
+    if (city) {
+        filteredData = filteredData.filter(ufo => ufo.city === city);
+    }
+    if (shape) {
+        filteredData = filteredData.filter(ufo => ufo.shape === shape);
+    }
+
+    filteredData.forEach(function(filter) {
+        console.log(filter);
+        var row = tbody.append("tr");
+        Object.entries(filter).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
